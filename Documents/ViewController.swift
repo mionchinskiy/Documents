a80f33111b7cfdb72cd0f470a5290485aaec685f
Mionchinskiy <d.mionchinskiy@gmail.com>
2023-07-20 13:18:53 +0300

    Initial Commit

---
+++ Documents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Documents
+//
+//  Created by Denis Mionchinskiy on 20.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
